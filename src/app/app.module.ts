6529f699d1346a493fbea462c8c5f09a9232b13c
chai <chaithra.amin98@gmail.com>
2022-10-10 10:40:27 +0530

    added home details page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ListingComponent } from './listing/listing.component';
+import { HouseDetailPageComponent } from './house-detail-page/house-detail-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ListingComponent
+    ListingComponent,
+    HouseDetailPageComponent
   ],
   imports: [
     BrowserModule,
